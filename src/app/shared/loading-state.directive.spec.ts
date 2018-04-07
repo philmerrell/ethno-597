d25305b37a530beb9c688a2be9268f85fa7ddb96
Phil Merrell <philmerrell@boisestate.edu>
2018-04-07 10:13:33 -0600

    fix: Remove loading state directive

--- src/app/shared/loading-state.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LoadingStateDirective } from './loading-state.directive';
-
-describe('LoadingStateDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LoadingStateDirective();
-    expect(directive).toBeTruthy();
-  });
-});
